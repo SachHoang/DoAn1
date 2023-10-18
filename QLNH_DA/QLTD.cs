df94ea739f4ab413f8d55a66ea067859d46bfec6
nguyenchinhtan2003@gmail.com <nguyenchinhtan2003@gmail.com>
2023-10-18 08:58:26 +0700

    code td

--- QLNH_DA/QLTD.cs
+++ QLNH_DA/QLTD.cs
@@ -20,7 +20,30 @@ namespace QLNH_DA
 
         private void QLTD_Load(object sender, EventArgs e)
         {
-           
+            Model1 context = new Model1();
+            List<QuanLiThucDon> qltd = context.QuanLiThucDons.ToList();
+            List<QuanLiNhomMonAn> qlnma  = context.QuanLiNhomMonAns.ToList();
+            FillNhomCombobox(qlnma);
+            BindGrid(qltd);
+
+        }
+        private void FillNhomCombobox(List<QuanLiNhomMonAn> qlnma)
+        {
+            this.cmbNhom.DataSource = qlnma;
+            this.cmbNhom.DisplayMember = "TenNhom";
+            this.cmbNhom.ValueMember = "TenNhom";
+        }
+        private void BindGrid(List<QuanLiThucDon> qltd)
+        {
+            dgvQLTD.Rows.Clear();
+            foreach(var item in qltd)
+            {
+                int index = dgvQLTD.Rows.Add();
+                dgvQLTD.Rows[index].Cells[1].Value = item.MonAnID;
+                dgvQLTD.Rows[index].Cells[2].Value = item.TenMon;
+                dgvQLTD.Rows[index].Cells[3].Value = item.Gia;
+                dgvQLTD.Rows[index].Cells[4].Value = item.QuanLiNhomMonAn.TenNhom;
+            }
         }
         
     }
